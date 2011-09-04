9cef69faee25428546601e62eae0b560b9956a4f
David Thevenin <david.thevenin@gmail.com>
2011-09-04 23:43:53 +0200

    Add PhoneGap read events fire
    Add support of onNativeReady, onPhoneGapInfoReady and
    onPhoneGapConnectionReady, events firing after PhoneGap is ready

--- framework/webpage.cpp
+++ framework/webpage.cpp
@@ -6,24 +6,31 @@
 #include <QWebFrame>
 
 
-WebPage::WebPage(QObject *parent) :
-    QWebPage(parent) {
+WebPage::WebPage (QObject *parent) :
+  QWebPage(parent) {
 
-    setNetworkAccessManager(new NetworkAccessManager(this));
+  setNetworkAccessManager (new NetworkAccessManager(this));
 
-    connect(this, SIGNAL(loadFinished(bool)), SLOT(loadFinished(bool)));
+  connect (this, SIGNAL(loadFinished(bool)), SLOT(loadFinished(bool)));
 }
 
-void WebPage::javaScriptConsoleMessage(const QString &message, int lineNumber, const QString &sourceId) {
-    Q_UNUSED(lineNumber);
-    Q_UNUSED(sourceId);
+void WebPage::javaScriptConsoleMessage (const QString &message, int lineNumber, const QString &sourceId) {
+  Q_UNUSED (lineNumber);
+  Q_UNUSED (sourceId);
 
-    qDebug() << message;
+  qDebug() << message;
 }
 
-void WebPage::loadFinished(bool ok) {
-
-    if (!ok) {
-        QMessageBox::warning(0, "Error", "There was an error loading the page " + mainFrame()->url().toString());
-    }
+void WebPage::loadFinished(bool ok)
+{
+  if (!ok) {
+    QMessageBox::warning(0, "Error", "There was an error loading the page " + mainFrame()->url().toString());
+  }
+  else
+  {
+    QWebFrame *frame = mainFrame ();
+    frame->evaluateJavaScript ("javascript:try{ PhoneGap.onNativeReady.fire();}catch(e){_nativeReady = true;}");
+    frame->evaluateJavaScript ("javascript:try{ PhoneGap.onPhoneGapInfoReady.fire();}catch(e){console.log (e);}");
+    frame->evaluateJavaScript ("javascript:try{ PhoneGap.onPhoneGapConnectionReady.fire();}catch(e){console.log (e);}");
+  }
 }
